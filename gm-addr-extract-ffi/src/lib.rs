810b165219e1d0e44eaefe3103667a6c68a569cf
poly000 <1348292515@qq.com>
2025-01-17 11:10:52 +0800

    docs: generate doc comments to C/C++ header [ci skip]

--- gm-addr-extract-ffi/src/lib.rs
+++ gm-addr-extract-ffi/src/lib.rs
@@ -7,19 +7,26 @@ use std::{
 
 use gm_addr_extract_lib::patterns::AdrpAdrpStrStr;
 
+/// Extract RVA of a pointer to GlobalMetadata,
+///
+/// using `libil2cpp.so` from current process.
+///
+/// (use with zygisk hook, e.g.)
 #[no_mangle]
 extern "C" fn rva_from_current_process() -> u64 {
     _rva_from_current_process().map(NonZero::into).unwrap_or(0)
 }
 
+/// Extract RVA of a pointer to GlobalMetadata,
+///
+/// using `libil2cpp.so` from `file_path` (encoded as UTF-8).
+///
+/// You can deallocate `file_path` after this call.
+///
+/// `len` means bytes of the string (without the possible trailing null).
+///
+/// SAFETY: you must pass a valid UTF-8 encoded string.
 #[no_mangle]
-/*
-SAFETY: you must pass null-terminated, UTF-8 encoded string.
-
-You must deallocate it yourself.
-
-`len` means bytes of the string, without the trailing null.
-*/
 unsafe extern "C" fn rva_from_path(file_path: *const u8, len: usize) -> u64 {
     let utf8_slice = unsafe { slice::from_raw_parts(file_path as _, len) };
     let path = String::from_utf8_lossy(utf8_slice);
--- gmextra.h
+++ gmextra.h
@@ -1,6 +1,6 @@
 #pragma once
 
-/* Generated with cbindgen:0.27.0 */
+/* Generated with cbindgen:0.28.0 */
 
 #include <stdarg.h>
 #include <stdbool.h>
@@ -11,8 +11,26 @@
 extern "C" {
 #endif // __cplusplus
 
+/**
+ * Extract RVA of a pointer to GlobalMetadata,
+ *
+ * using `libil2cpp.so` from current process.
+ *
+ * (use with zygisk hook, e.g.)
+ */
 uint64_t rva_from_current_process(void);
 
+/**
+ * Extract RVA of a pointer to GlobalMetadata,
+ *
+ * using `libil2cpp.so` from `file_path` (encoded as UTF-8).
+ *
+ * You can deallocate `file_path` after this call.
+ *
+ * `len` means bytes of the string (without the possible trailing null).
+ *
+ * SAFETY: you must pass a valid UTF-8 encoded string.
+ */
 uint64_t rva_from_path(const uint8_t *file_path, uintptr_t len);
 
 #ifdef __cplusplus
